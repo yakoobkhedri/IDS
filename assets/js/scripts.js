ed1d2ca400d2b34e28798016706455af3553d6de
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-17 21:23:48 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -53,3 +53,34 @@ var testimonials = new Swiper(".testimonials", {
         prevEl: ".swiper-button-prev",
     },
   });
+  
+var swiper = new Swiper(".smallImgs", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints:{
+        1200:{
+            slidesPerView: 3,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+        576:{
+            slidesPerView: 3,
+        },
+        400:{
+            slidesPerView: 3,
+        },
+    }
+});
+var swiper2 = new Swiper(".gallery", {
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
