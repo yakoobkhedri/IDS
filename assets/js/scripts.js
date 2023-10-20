60a69328641997570055629bf8e8f466ded3887d
yakoobkhedri <yakkobkhedrii@gmail.com>
2023-10-20 20:51:42 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,23 @@
+// mobile menu
+
+let hamIcon=document.getElementById('hamIcon');
+let menu=document.getElementById('menu');
+let closemenu=document.getElementById('closemenu');
+let overlay=document.getElementById('overlay');
+
+hamIcon.addEventListener('click',function () {
+    menu.classList.add('active');
+    overlay.classList.add('active');
+})
+closemenu.addEventListener('click',function () {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+})
+overlay.addEventListener('click',function () {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+})
+
 // aos
 AOS.init();
 
@@ -53,34 +73,3 @@ var testimonials = new Swiper(".testimonials", {
         prevEl: ".swiper-button-prev",
     },
   });
-  
-var swiper = new Swiper(".smallImgs", {
-    spaceBetween: 10,
-    slidesPerView: 3,
-    freeMode: true,
-    watchSlidesProgress: true,
-    breakpoints:{
-        1200:{
-            slidesPerView: 3,
-        },
-        768:{
-            slidesPerView: 3,
-        },
-        576:{
-            slidesPerView: 3,
-        },
-        400:{
-            slidesPerView: 3,
-        },
-    }
-});
-var swiper2 = new Swiper(".gallery", {
-    spaceBetween: 0,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-        swiper: swiper,
-    },
-});
