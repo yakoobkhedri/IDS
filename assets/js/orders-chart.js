4910dc5dc08860dd7a318f463e02678afe1b9ac6
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-05-28 17:32:13 +0330

    commit message

--- assets/js/orders-chart.js
+++ assets/js/orders-chart.js
@@ -4,9 +4,9 @@ const ctx2 = document.getElementById('myChart2');
 new Chart(ctx, {
   type: 'pie',
   data: {
+    labels: ['تکمیل شده','لغو', 'در حال پردازش'],
     datasets: [{
-      label: '# of Votes',
-      data: [20, 10, 3]
+      data: [20, 3, 10]
     }]
   },
   options: {
@@ -28,25 +28,15 @@ new Chart(ctx, {
 new Chart(ctx2, {
   type: 'line',
   data: {
-    labels: ['جمع کل', 'تکمیل شده', 'در حال انتظار', 'باز پرداخت شده', 'لغو شده'],
+    labels: ['1', '2', '3', '4', '5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'],
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2]
+      fontColor: '#64CCC5',
+      label: 'فروش این ماه',
+      data: [200, 300, 100, 300, ,200,100, 300, 500, 400, ,200,100, 300, 500, 500, ,100,200, 400, 500, 500, ,200,200, 300, 500, 500, ,200,200, 300, 500, 500, ,200,400]
     }]
   },
   options: {
-
-    legend: {
-
-      display: true,
-      labels: {
-        fontColor: '#FFFFFF'
-      },
-      position: 'bottom'
-    },
-    title: {
-      display: true,
-    }
-  }
-
+   
+  },
+  
 });
